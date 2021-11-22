c452bc913a82e4a7a1b37110f37e194d895fb926
Graham Walker <gwalker256@gmail.com>
2021-11-22 15:43:56 +0000

    fixed javascript file

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
-const shoppingList = ["Carrots", "Broccoli", "Cauliflower"]
-["Apples", "Pears", "Oranges"]
-["Toilet Paper", "Tissues", "Kitchen Towel"];
-
-console log shoppingList
+const shoppingList = [
+    veg = ["Carrots", "Broccoli", "Cauliflower"],
+    fruit = ["Apples", "Pears", "Oranges"],
+    ambient = ["Toilet Paper", "Tissues", "Kitchen Towel"]];
+    
+    console.log(shoppingList[1][2])
